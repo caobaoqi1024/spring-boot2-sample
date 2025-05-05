f4145ddd789b9d72e6064df6b870ba6b95bf0e9a
caobaoqi1024 <caobaoqi1024@gmail.com>
2025-05-05 18:00:00 +0800

    :heavy_plus_sign: feat(build): add basic dev dep

--- src/main/java/dev/cbq/backend/BackendApplication.java
+++ src/main/java/dev/cbq/backend/BackendApplication.java
@@ -1,11 +1,30 @@
 package dev.cbq.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisTemplate;
 
+import javax.sql.DataSource;
+
+@Slf4j
 @SpringBootApplication
 public class BackendApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+
+    @EventListener
+    public void onApplicationReady(ApplicationReadyEvent event) {
+        log.info("BackendApplication.onApplicationReady");
+        ConfigurableApplicationContext context = event.getApplicationContext();
+        log.info(String.valueOf(context.getBean("redisTemplate",RedisTemplate.class)));
+        log.info(String.valueOf(context.getBean("stringRedisTemplate",RedisTemplate.class)));
+        log.info(String.valueOf(context.getBean(DataSource.class)));
     }
 }
